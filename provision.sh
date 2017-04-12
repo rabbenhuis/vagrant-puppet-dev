efa4c5ef30e8665bfbe37f9de975097c4155bf13
Richard Abbenhuis <richard@abbenhuis.net>
2017-04-12 11:23:51 +0200

    Install salt

--- provision.sh
+++ provision.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+# Install salt
+if ! [[ $(command -v salt-call) ]]; then
+	echo "Install the SaltStack repository (latest)"
+	yum install https://repo.saltstack.com/yum/redhat/salt-repo-latest-1.el6.noarch.rpm -y
+	yum clean expire-cache
+	yum install salt-minion -y
+fi
